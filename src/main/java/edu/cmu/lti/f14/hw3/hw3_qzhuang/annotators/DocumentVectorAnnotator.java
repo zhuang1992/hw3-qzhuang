a69f18471c06cbf95f2607f73da0e83107ad2878
zhuang1992 <zhuang1992@gmail.com>
2014-10-14 14:55:12 -0400

    Initial Commit

---
+++ src/main/java/edu/cmu/lti/f14/hw3/hw3_qzhuang/annotators/DocumentVectorAnnotator.java
@@ -0,0 +1,41 @@
+package edu.cmu.lti.f14.hw3.hw3_qzhuang.annotators;
+
+import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.FSIterator;
+import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.IntegerArray;
+import org.apache.uima.jcas.cas.StringArray;
+import org.apache.uima.jcas.tcas.Annotation;
+
+import edu.cmu.lti.f14.hw3.hw3_qzhuang.typesystems.Document;
+
+public class DocumentVectorAnnotator extends JCasAnnotator_ImplBase {
+
+	@Override
+	public void process(JCas jcas) throws AnalysisEngineProcessException {
+
+		FSIterator<Annotation> iter = jcas.getAnnotationIndex().iterator();
+		if (iter.isValid()) {
+			iter.moveToNext();
+			Document doc = (Document) iter.get();
+			createTermFreqVector(jcas, doc);
+		}
+
+	}
+	/**
+	 * 
+	 * @param jcas
+	 * @param doc
+	 */
+
+	private void createTermFreqVector(JCas jcas, Document doc) {
+
+		String docText = doc.getText();
+		
+		//TO DO: construct a vector of tokens and update the tokenList in CAS
+		
+
+	}
+
+}
